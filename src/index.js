8215f32b40f80eaac468a598e5c66c76509e6bd4
Vlad Horban <ywvvvw@gmail.com>
2021-12-15 16:45:20 +0200

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function toReadable (number) {
-  
-}
+  const first = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
+  const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const mad = ['', 'thousand', 'million', 'billion', 'trillion'];
+  let word = '';
+
+  if(!number){
+    return 'zero';
+  }
+
+  let tempNumber = number % 100;
+
+  if (Math.floor(tempNumber) < 20) {
+    word = first[Math.floor(tempNumber)] + ' ' + word;
+  } else {
+    word = tens[Math.floor(tempNumber / 10)] + ' ' + first[Math.floor(tempNumber) % 10] + ' ' + word;
+  }
+
+  tempNumber = number % 1000;
+
+  if (Math.floor(tempNumber / 100)) {
+    word = first[Math.floor(tempNumber / 100)] + 'hundred ' + word;
+  }
+
+  return word.trim();
+};
